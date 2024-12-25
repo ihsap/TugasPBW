dfeb0df247ccef15af74009da77e573a3c7eb7a9
ihsann <ihsanadiputra8@gmail.com>
2024-12-25 18:42:43 +0700

    add tgs 11

---
+++ Tugas 11_Video 18-21/sticky/app/Http/Controllers/StoreController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRequest;
+use App\Models\Store;
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('stores.index', [
+            'stores'=>Store::latest()->get(),
+            
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('stores.form',[
+            'store' => new Store(),
+            'page_meta'=> [
+                'title'=>'Create store',
+                'description'=>'Create new store for yours.',
+                'method'=>'post',
+                'url'=>route('stores.store'),
+            ]
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        
+        $file = $request->file('logo');
+
+        $request->user()->stores()->create([
+            ...$request->validated(),
+            ...[
+                'logo' => $file->store('images/stores')
+            ],
+        ]);
+
+        return to_route('stores.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Store $store)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Request $request, Store $store)
+    {
+        abort_if($request->user()->isNot($store->user), 401);
+        return view('stores.form',[
+            'store' => $store,
+            'page_meta'=> [
+                'title'=>'Edit store',
+                'description'=>'Edit store:'.$store->name,
+                'method'=>'put',
+                'url'=>route('stores.update', $store),
+            ]
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StoreRequest $request, Store $store)
+    {
+        $store->update([
+            'name'=>$request->name,
+            'description'=>$request->description,
+        ]);
+
+        return to_route('stores.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Store $store)
+    {
+        //
+    }
+}
---
+++ Tugas 11_Video 18-21/sticky/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'IhsanAP',
+            'email' => 'ihsan9@gmail.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
+        ]);
+    }
+}
